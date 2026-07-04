a218c844535e28a8697cf10af7baaecbbd7d3378
Aradhya Rastogi <aradhyarast@gmail.com>
2026-07-04 21:47:57 +0530

    add day1

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+import pyjokes
+joke = pyjokes.get_jokes();
+print(joke)
+
+print('''
+Catch me if you can, working on my tan
+Salvatore
+Dying by the hand of a foreign man happily
+Calling out my name in the summer rain
+Ciao, amore
+Salvatore can wait
+Now it's time to eat soft ice cream (Soft ice cream)
+
+[Chorus]
+Ah-ah-ah-ah, ah-ah-ah-ah-ah
+Cacciatore
+Ah-ah-ah-ah, ah-ah-ah-ah-ah
+Limousines
+Ah-ah-ah-ah, ah-ah-ah-ah
+Ciao, amore
+Ah-ah-ah-ah, ah-ah-ah-ah-ah
+Soft ice cream
+    ''')
