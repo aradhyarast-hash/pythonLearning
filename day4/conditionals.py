0dbebfa0f22ba48fe4925c797bef1e759412bb36
Aradhya Rastogi <aradhyarast@gmail.com>
2026-07-08 20:18:21 +0530

    add day4

---
+++ day4/conditionals.py
@@ -0,0 +1,27 @@
+# if elif else
+
+a = 90
+if(a > 18):
+    print("eligible")
+    print("you can vote")
+elif(a == 18):
+    print("sorry not eligible")
+    print("you are not allowed")
+
+elif(a < 0):
+    print("enter a valid age")
+
+else:
+    print("not eligible")
+    print("you can't vote")
+
+
+print("thank you")
+
+
+# logical operators in python
+
+# 1. and
+# 2. or
+# 3. not
+
---
+++ day4/loops.py
@@ -0,0 +1,21 @@
+# for i in range(0,10):
+#     print(i)
+
+# for i in range(1,11):
+#     print(2 * i)
+
+# i = 1
+# while(i < 19):
+#     print(i, "is printed")
+#     i += 1
+
+
+# l = ["demon", "caroline", "elijah", "hayley", "katherine"]
+# for i in l:
+#     if(i.startswith('d')):
+#         print(f"hello {i}")
+#     else:
+#         print("hello stranger")
+
+for i in range(1,10):
+    pass
