11ea95d8042614a16e4bebd0531eec2ae7d6922b
Aradhya Rastogi <aradhyarast@gmail.com>
2026-09-13 23:02:56 +0530

    first api build

--- day18/consumeapi.py
+++ day18/consumeapi.py
@@ -2,6 +2,6 @@ import requests
 
 age = 15
 sex = 'F'
-response = request.get(f"http://127.0.0.1:8000/predict?age={age}&sex={sex}")
+response = requests.get(f"http://127.0.0.1:8000/predict?age={age}&sex={sex}")
 output = response.json()
 print(output)
